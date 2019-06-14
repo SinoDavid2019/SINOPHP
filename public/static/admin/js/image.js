e330019ff80f3c83351ad328b8dc17f63ba0e5b9
sino <15294182360@163.com>
2019-06-14 15:49:09 +0800

    图片上传

--- public/static/admin/js/image.js
+++ public/static/admin/js/image.js
@@ -1,6 +1,7 @@
 $(function() {
     $("#file_upload").uploadify({
         swf     :swf,
-        uploader:image_upload_url
+        uploader:image_upload_url,
+        buttonText:'上传图片'
     });
 });
