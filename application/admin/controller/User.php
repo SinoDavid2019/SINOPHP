504799d78e13346bbf158f6cf8b7e972ee0e5e22
sino <15294182360@163.com>
2019-07-12 10:51:16 +0800

    用户分页查询

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -9,6 +9,7 @@
 namespace app\admin\controller;
 
 
+use app\common\lib\IAuth;
 use think\Controller;
 use app\admin\model\User as UserModel;
 
@@ -29,6 +30,59 @@ class User extends Controller
         $this->assign("data",$data);
         $this->assign("num",$num);
         return $this->fetch("user-list");
+    }
+
+    public function  addUser(){
+
+        if($this->request->isPost()){
+            $insert_arr=[];
+            $insert_arr["user_name"]=input("user_name");
+            $insert_arr["password"]=IAuth::setPassword(input("passsword"));
+            $insert_arr["email"]=input("email");
+            $insert_arr["phone"]=input("phone");
+            $insert_arr["age"]=input("age");
+            $insert_arr["status"]=input("status");
+            $insert_arr["last_login_ip"]=$this->request->ip();
+            $insert_arr["last_login_time"]=date("Y-m-d H:i:s",time());
+            $insert_arr["create_time"]=time();
+            $insert_arr["update_time"]=time();
+            $browser=$_SERVER['HTTP_USER_AGENT'];
+            $insert_arr["user_browser"]=$browser;
+            $insert_arr["user_logo"]=input("user_logo");
+            if (!empty($browser)) {
+                if (preg_match('/win/i', $browser)) {
+                    $insert_arr["user_system"]="Windows";
+                } else if (preg_match('/mac/i', $browser)) {
+                    $insert_arr["user_system"]="Mac";
+                } else if (preg_match('/linux/i', $browser)) {
+                    $insert_arr["user_system"]="Linux";
+                } else if (preg_match('/unix/i', $browser)) {
+                    $insert_arr["user_system"]="Unix";
+                } else if (preg_match('/bsd/i', $browser)) {
+                    $insert_arr["user_system"]="BSD";
+                } else {
+                    $insert_arr["user_system"]="没有此系统";
+                }
+            } else {
+                return 'unknow';
+            }
+
+            $result=model('User')->insert($insert_arr);
+
+            if($result){
+                $this->success("添加用户成功");
+            }else{
+                $this->error('添加用户失败');
+            }
+
+
+
+
+
+        }
+
+        return $this->fetch("user-add");
+
     }
 
 }
--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -16,11 +16,11 @@ class User extends Model
 
     public function user_list($where,$min,$max){
         if($min&&$max){
-            $result=$this->where($where)->whereTime("create_time","between",[$min,$max])->where('rective','=',1)->select();
+            $result=$this->where($where)->whereTime("create_time","between",[$min,$max])->where('status','=',1)->select();
 
             return $result;
         }else{
-            $result=$this->where($where)->where('rective','=',1)->select();
+            $result=$this->where($where)->where('status','=',1)->select();
 
             return $result;
         }
